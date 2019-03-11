d69ea88807fa80142bedc8c3c42b61b1a77df347
Maxwell Heeschen <gorpmaster@gmail.com>
2019-03-10 21:19:22 -0500

    Impelement parts of new design.
    I went back to the drawing board and now have a much better
    understanding of how the canvas stuff works, as well as some ideas for
    handling multiple "objects" being drawn onto it at once. I'm getting
    some errors that I need to handle though. I also need to implement the
    actual drawing of objects in an array, as well as picking a random word
    to add to the screen.

--- main.js
+++ main.js
@@ -1,21 +1,59 @@
-var canvas = document.getElementById("canvas"),
-    context = canvas.getContext("2d"),
-    width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight;
-    textboxHeight = document.getElementById("textbox").offsetHeight;
-
-canvas.height -= textboxHeight;
-
-var targetList = {};
-
-function target(word, xPos, yPos) {
-    this.word = word;
-    this.xPos = xPos;
-    this.yPos = yPos;
+function main() {
+    gameArea.start();
 }
-var temp target("test", 5, 5);
-targetList["test"] = temp;
-alert(targetList["test"]);
 
-delete targetList.test;
-alert(targetList["test"]);
+function updateGame() {
+    gameArea.clear();
+    wordTargetList.moveList();
+    wordTargetList.check();
+    wordTargetList.draw();
+}
+
+var gameArea = {
+    canvas: document.getElementById("canvas"),
+    textbox: document.getElementById("textbox"),
+    start: function() {
+        this.context = this.canvas.getContext("2d");
+        this.interval = setInterval(updateGame(), 20);
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function() {
+        clearInterval(this.interval);
+    }
+}
+
+function wordTarget(word, x, y, width, height, color) {
+    this.word = word,
+    this.x = x,
+    this.y = y,
+    this.width = width,
+    this.height = height,
+    this.color = color,
+    this.update = function() {
+        context = gameArea.context;
+        context.fillStyle = color
+    }
+}
+
+var wordTargetList = {
+    components: [],
+    moveList: function() {
+        for(i = 0; i < components.length; i++) {
+            components[i].x -= 1;
+        }
+    },
+    check: function() {
+        for(i = 0; i < components.length; i++) {
+            if(components[i].x < 0) {
+                gameArea.stop();
+                break;
+            }
+        }
+    },
+    draw: function() {
+        for(i = 0; i < components.length; i++){
+        }
+    }
+}
